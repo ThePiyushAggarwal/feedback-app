b3ed25fcce15fbe18434c5d039d797d6db0acd22
Piyush Aggarwal <piyushaggarwal801@gmail.com>
2022-03-08 15:44:18 +0530

    slowly progressing

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,6 +1,7 @@
 import FeedbackItem from './FeedbackItem'
+import PropTypes from 'prop-types'
 
-function FeedbackList({ feedback }) {
+function FeedbackList({ feedback, handleDelete }) {
   if (!feedback || feedback.length === 0) {
     return <p>'No Feedback Yet'</p>
   }
@@ -8,10 +9,20 @@ function FeedbackList({ feedback }) {
   return (
     <div className="feedback-list">
       {feedback.map((item) => (
-        <FeedbackItem item={item} key={item.id} />
+        <FeedbackItem item={item} key={item.id} handleDelete={handleDelete} />
       ))}
     </div>
   )
+}
+
+FeedbackList.propTypes = {
+  feedback: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      rating: PropTypes.number.isRequired,
+      text: PropTypes.string.isRequired,
+    })
+  ),
 }
 
 export default FeedbackList
